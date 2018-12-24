b820fa6187d1d7061ef089744bb6ca79012478f4
tyiki badwell <tyiki.badwell@gmail.com>
2018-12-25 01:09:44 +0900

    余計なcss,js削除

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -37,6 +37,7 @@ namespace WebApplication1.Controllers
             return View("Index");
         }
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
